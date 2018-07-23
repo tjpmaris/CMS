fb5d5f2bc5c9e50ee6515aa26b46f3fa151eec60
Jake <centerback_rules@yahoo.com>
2018-07-23 10:34:13 -0600

    added admin user to database

--- cmsSQL.sql
+++ cmsSQL.sql
@@ -1,4 +1,4 @@
-Create database CMSPHP;
+Create database if not exists CMSPHP;
 use CMSPHP;
 
 CREATE TABLE IF NOT EXISTS users (
@@ -8,6 +8,9 @@ CREATE TABLE IF NOT EXISTS users (
   user_password varchar(250) not null,
   PRIMARY KEY (user_id)
 );
+
+insert into users(user_id, user_name, user_role, user_password)
+value(0,'admin','admin','password');
 
 create Table if Not exists Element(
 	element_id int(11) not null auto_increment,
