1be65c50fbcc5d8b288496f8f235ffea670d5947
lunarnublet <lunarnublet@gmail.com>
2018-07-23 09:58:17 -0600

    idk

--- front-end/index.php
+++ front-end/index.php
@@ -5,5 +5,17 @@
 
     <body>
         <p>Hello, World!<p>
+        <div class=container>
+            <div class=add-widget>
+            
+            </div>
+
+            <div class=main-content>
+
+            </div>
+            <div class=edit-widget>
+            
+            </div>
+        </div>
     </body>
 </html>
