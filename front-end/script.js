a5eb20d644e134678c9b22827afc7e983a9e20b3
Jake <centerback_rules@yahoo.com>
2018-07-25 10:46:12 -0600

    fix to script.js so that we have no more in-line scripts

--- front-end/script.js
+++ front-end/script.js
@@ -1,17 +1,4 @@
 console.log("Things");
-function addButtonClick(){
-    console.log("Starting");
-    var element = document.getElementById("add-widget");
-    console.log(element);
-    if(element.style.visibility === "hidden"){
-        element.style.visibility = "visible";
-        console.log("setting visible");
-    }
-    else{
-        element.style.visibility = "hidden";
-        console.log("setting hidden");
-    }
-}
 
 function myFunction() {
     var x = document.getElementById('myDIV');
@@ -22,6 +9,16 @@ function myFunction() {
     }
 }
 
-function addButtonClick(){
-    
+function visibleButtonClick(id){
+    console.log("Starting");
+    var element = document.getElementById(id);
+    console.log(element);
+    if(element.style.visibility === "hidden"){
+        element.style.visibility = "visible";
+        console.log("setting visible");
+    }
+    else{
+        element.style.visibility = "hidden";
+        console.log("setting hidden");
+    }
 }
