a04655bba1656ee7c0ae0df02e4a325d0e9bcf78
lunarnublet <lunarnublet@gmail.com>
2018-07-25 10:47:14 -0600

    things.

--- front-end/index.php
+++ front-end/index.php
@@ -5,22 +5,21 @@
     </head>
 
     <body>
-        <div class=container>
-            <div id=main-content class=main-content>
+        <div class="container">
+            <div id="main-content" class="main-content">
 
             </div>
 
-            <div id=add-widget class=add-widget >
+            <div id="add-widget" class="add-widget">
             
             </div>
 
-            <div id=edit-widget class=edit-widget>
+            <div id="edit-widget" class="edit-widget">
+                <button id="edit-button" class="edit-button" onclick="visibleButtonClick('edit-widget')">X</button>
 
             </div>
 
             <button id="add-button" class="add-button" onclick="visibleButtonClick('add-widget')">Add</button>
-
-            <button id="edit-button" class="edit-button" onclick="visibleButtonClick('edit-widget')">Edit</button>
 
 
 
