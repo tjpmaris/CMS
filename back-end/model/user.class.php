ae50039991a290b143c5d15cf67916b98e250e33
jouzune <dmatson@student.neumont.edu>
2018-07-26 08:30:38 -0600

    Add User model

---
+++ back-end/model/user.class.php
@@ -0,0 +1,26 @@
+<?php
+
+    class User {
+        private $id;
+        private $name;
+        private $role;
+
+        public function __construct(int $id, string $name, string $role) {
+            $this->id = $id;
+            $this->name = $name;
+            $this->role = $role;
+        }
+
+        public function __get($get) {
+            if ($get === 'id')
+                return $this->id;
+            if ($get === 'name')
+                return $this->name;
+            if ($get === 'role')
+                return $this->role;
+
+            throw Exception('Invalid property access');
+        }
+    }
+
+?>
