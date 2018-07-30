5ebf655d823317c3c96f6e2000b49f20147bf4ad
jouzune <dmatson@student.neumont.edu>
2018-07-30 13:37:55 -0600

    Add MySQL connection

---
+++ back-end/database/mysql.php
@@ -0,0 +1,26 @@
+<?php
+
+$DB_CONFIG = array(
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db_name' => 'cmsphp'
+);
+
+function db_connection($db_config)
+{
+    $mysqli = new mysqli(
+        $db_config['host'],
+        $db_config['username'],
+        $db_config['password'],
+        $db_config['db_name']
+    );
+    
+    if(mysqli_connect_errno()) {
+        throw new Exception('Error: Could not connect to database. ' . mysqli_connection_errno());
+    }
+
+    return $mysqli;
+}
+
+?>
