1d3938ee66bcdb16ce8b0e44a3a6c803c241110f
Jake <centerback_rules@yahoo.com>
2018-08-06 11:56:31 -0600

    dont know why i cannot do the second query

--- back-end/database/ElementClass.php
+++ back-end/database/ElementClass.php
@@ -1,22 +1,22 @@
 <?php
-    class Element{
-        $elementName;
-        $elementId;
-        $elementColor;
-        $elementBackground;
-        $elementType;
-        $elementWebpageId;
-        $elementFont;
-        $elementSize;
-        public function __construct($elementName,$elementId, $elementWebpageId, $elementColor, $elementBackground
-        $elementType,$elementFont,$elementSize){
-            $this->elementName = $elementName;
-            $this->elementId = $elementId;
-            $this->elementWebpageId = $elementWebpageId;
-            $this->elementColor = $elementColor;
-            $this->elementType = $elementType;
-            $this->elementFont = $elementFont;
-            $this->elementSize = $elementSize;
-        }
-    }
+    // class Element{
+    //     $elementName;
+    //     $elementId;
+    //     $elementColor;
+    //     $elementBackground;
+    //     $elementType;
+    //     $elementWebpageId;
+    //     $elementFont;
+    //     $elementSize;
+    //     public function __construct($elementName,$elementId, $elementWebpageId, $elementColor, $elementBackground
+    //     $elementType,$elementFont,$elementSize){
+    //         $this->elementName = $elementName;
+    //         $this->elementId = $elementId;
+    //         $this->elementWebpageId = $elementWebpageId;
+    //         $this->elementColor = $elementColor;
+    //         $this->elementType = $elementType;
+    //         $this->elementFont = $elementFont;
+    //         $this->elementSize = $elementSize;
+    //     }
+    // }
 ?>
--- back-end/database/Webpage.php
+++ back-end/database/Webpage.php
@@ -1,8 +1,8 @@
 <?php
     include 'mysql.php';
     include 'WebpageClass.php';
-    include 'ElementClass.php';
-    db_connection();
+    // include 'ElementClass.php';
+    $mysqli = db_connection($DB_CONFIG);
     //query all records from the database
     $query_webpages = "select * from webpage";
     $webPages = array();
@@ -16,27 +16,33 @@
         while( $row = $pages_result->fetch_assoc() ){
             extract($row);
             header("Access-Control-Allow-Origin: *");
-            array_push($webPages, new WebpageClass($web_name, $webpage_id));
+            echo $webpage_id." ".$webpage_name." ".$webpage_filename." ".$webpage_filepath."<br/>";
+            array_push($webPages, new Webpage($webpage_name, $webpage_id));
         }
     }else{
         //if database table is empty
     
     }
     //disconnect from database
-    $result->free();
+    $pages_result->free();
     $mysqli->close();
+
+    $mysql = db_connection($DB_CONFIG);
     $query_elements = "select * from elements";
-    $elements_result = $mysqli->query( $query_elements );
-    $num_results = $elements_result->num_rows;
-    if( $num_results > 0){ 
-          while( $row = $pages_result->fetch_assoc() ){
+    $elements_result = $mysql->query( $query_elements );
+    echo $elements_result."resutlsa;hgdks";
+    // echo $elements_result->num_rows;
+    // $num_eleresults = $elements_result->num_rows;
+    if(true){//$num_eleresults > 0){ 
+          while( $row = $elements_result->fetch_assoc() ){
               extract($row);
               header("Access-Control-Allow-Origin: *");
-              $webPages[$webpage_id]->addElementToPage(new ElementClass($element_id, $element_name))
+              echo $webpage_id." ".$element_name." ".$element_id." ".$element_content."<br/>";
+              //$webPages[$webpage_id]->addElementToPage(new ElementClass($element_id, $element_name));
           }
     }else{
       
     }
-    $result->free();
-    $mysqli->close();
+    $elements_result->free();
+    $mysql->close();
 ?>
