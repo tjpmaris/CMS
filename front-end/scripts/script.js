38fce01810b973db90378d7e51f1ea856f77052d
lunarnublet <lunarnublet@gmail.com>
2018-08-12 20:05:39 -0600

    Crud operations work. Creating a page isn't a thing yet.

--- front-end/scripts/script.js
+++ front-end/scripts/script.js
@@ -2,14 +2,14 @@ var elements = [];
 var elementsToRemove = [];
 var elementsToAdd = [];
 var pages = [];
-var pageToRemove;
 var pageToAdd;
 var mainPages = [];
 var childPages= [];
 var currentPageName;
 var currentPage;
-var editing = true;
-var request = new XMLHttpRequest();    
+var editing = false;
+var allTypes = ["p", "h1"];
+var request = new XMLHttpRequest();
 
 function getData(){
     var url = 'http://localhost/php/cms/back-end/database/Webpage.php';
@@ -43,17 +43,19 @@ function webpagesCallback(){
 function addElementsCallback(elements){
     if(editing){
         addPageDeleteButton();
-        // startForm();
         elements.forEach(addFormElements);
+        addAddButton();
         addSaveButton();
     } else{
         elements.forEach(addElement);
+        addEditButton();
     }
 }
 
 function addPageDeleteButton(){
     var header = document.getElementById("NavBar");
     var button = document.createElement("button");
+
     button.innerHTML = "Remove Page";
     button.addEventListener('click', deletePageClick);
     header.appendChild(button);
@@ -63,14 +65,77 @@ function addPageDeleteButton(){
 }
 
 function addSaveButton(){
-    var header = document.getElementById("main-content");
+    var header = document.getElementById("button-div");
     var button = document.createElement("button");
+
     button.innerHTML = "Save";
     button.addEventListener('click', saveClick);
     header.appendChild(button);
     
     var btag = document.createElement("br");
     header.appendChild(btag);
+}
+
+function addAddButton(){
+    var header = document.getElementById("button-div");
+    var button = document.createElement("button");
+
+    button.innerHTML = "Add";
+    button.addEventListener('click', addClick);
+    header.appendChild(button);
+    
+    var btag = document.createElement("br");
+    header.appendChild(btag);
+}
+
+function addClick(){
+    var elementName = "New Element " + (elementsToAdd.length + 1);
+    var form = document.getElementById("main-content");
+    var div = document.createElement("div");
+    div.id = elementName;
+    div.style.borderBottom = "1px solid #000000";
+    form.appendChild(div);
+    
+    var nameInput = document.createElement("input");
+    nameInput.value = elementName;
+    div.appendChild(nameInput);
+    
+    var text = document.createElement("textarea");
+    var textContent = document.createTextNode("Input your text here");
+    text.appendChild(textContent);
+    div.appendChild(text);
+
+    var dropdown = document.createElement("select");
+    allTypes.forEach((item, index) => dropdown.options.add(new Option(item, item)));
+    dropdown.selectedIndex = 0;
+    div.appendChild(dropdown);
+    
+    var button = document.createElement("button");
+    button.addEventListener('click', deleteElementClick);
+    button.innerHTML = "Delete";
+    div.appendChild(button);
+
+    var breakTag = document.createElement("br");    
+    div.appendChild(breakTag)
+
+    elementsToAdd.push(elementName);
+}
+
+function addEditButton(){
+    var header = document.getElementById("main-content");
+    var button = document.createElement("button");
+
+    button.innerHTML = "Edit";
+    button.addEventListener('click', editClick);
+    header.appendChild(button);
+    
+    var btag = document.createElement("br");
+    header.appendChild(btag);
+}
+
+function editClick(){
+    editing = true;
+    softRefresh();
 }
 
 function deletePageClick(){
@@ -102,7 +167,6 @@ function startForm() {
 
 function addFormElements(item, index){
     var form = document.getElementById("main-content");
-    // var form = document.getElementById("edit-form");
     var div = document.createElement("div");
     div.id = item.name;
     div.style.borderBottom = "1px solid #000000";
@@ -112,6 +176,9 @@ function addFormElements(item, index){
     var labelContent = document.createTextNode(item.name);
     label.appendChild(labelContent);
     div.appendChild(label);
+    
+    var dropdown = document.createElement("select");
+    allTypes.forEach((item, index) => dropdown.options.add(new Option(item, item)));
 
     if(item.type === "p"){
         var text = document.createElement("textarea");
@@ -119,13 +186,19 @@ function addFormElements(item, index){
 
         text.appendChild(textContent);
         div.appendChild(text);
+
+        dropdown.selectedIndex = 0;
     } else {
         var input = document.createElement("input");
         input.type = "text";
         input.value = item.content
 
         div.appendChild(input);
+
+        dropdown.selectedIndex = 1;
     }
+
+    div.appendChild(dropdown);
     
     var button = document.createElement("button");
     button.addEventListener('click', deleteElementClick);
@@ -141,7 +214,6 @@ function deleteElementClick(evt){
     elementsToRemove.push(elementId);
 
     evt.target.parentElement.parentElement.removeChild(evt.target.parentElement);
-    console.log(elementsToRemove);
 }
 
 function addLinksCallback(mainPages, childPages){
@@ -178,8 +250,16 @@ function linkClick(evt){
     softRefresh();
 }
 
-function removePageElements() {
+function removeMainContentElements() {
     var div = document.getElementById("main-content");
+
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+function removeButtonDivElements() {
+    var div = document.getElementById("button-div");
 
     while (div.firstChild) {
         div.removeChild(div.firstChild);
@@ -195,7 +275,8 @@ function removeNavBarElements() {
 }
 
 function clearPage(){
-    removePageElements();
+    removeMainContentElements();
+    removeButtonDivElements();
     removeNavBarElements();
 }
 
@@ -204,7 +285,6 @@ function softRefresh(){
     elementsToRemove = [];
     elementsToAdd = [];
     pages = [];
-    pageToRemove = undefined;
     pageToAdd = undefined;
     mainPages = [];
     childPages= [];
@@ -218,7 +298,6 @@ function hardRefresh(){
     elementsToRemove = [];
     elementsToAdd = [];
     pages = [];
-    pageToRemove = undefined;
     pageToAdd = undefined;
     mainPages = [];
     childPages= [];
@@ -229,15 +308,80 @@ function hardRefresh(){
 }
 
 function saveClick(){
-    elementsToRemove.forEach(deleteElementCall);
-    elementsToRemove = [];
+    if(elementsToRemove.length > 0){
+        var id = elementsToRemove.pop();
+        var index = elements.indexOf(elements.find(s => s.id == id));
 
-    editing = false;
+        if (index > -1) {
+          elements.splice(index, 1);
+        }
+
+        deleteElementCall(id);
+    } else if(elements.length > 0){
+        var originalElement = elements.pop();
+        var element = { id: originalElement.id };
+
+        constructEditElement(originalElement.name, element);
+        updateElementCall(element);
+    } else if(elementsToAdd.length > 0){
+        var tempName = elementsToAdd.splice(0, 1);
+        var element = {webpageId: currentPage.details.id};
+        console.log(tempName);
+        constructAddElement(tempName, element);
+        console.log(element);
+        addElementCall(element);
+    } else{
+        editing = false;
+        hardRefresh();
+    }
 }
 
-function deleteElementCall(id, index){
-    var url = 'http://localhost/php/cms/back-end/database/deleteelement.php?id=' + id;
-    
-        request.open('GET', url);
-        request.send();
+function constructAddElement(elementName, element){
+    var div = document.getElementById(elementName);
+    var children = div.children;
+
+    element.name = children[0].value;
+    element.content = children[1].value;  
+    element.type = children[2].options[children[2].selectedIndex].value;
+}
+
+function constructEditElement(elementName, element){
+    var div = document.getElementById(elementName);
+    var children = div.children;
+
+    element.content = children[1].value;  
+    element.type = children[2].options[children[2].selectedIndex].value;
+}
+
+function addElementCall(element){
+    var url = 'http://localhost/php/cms/back-end/database/AddElement.php?';
+    url += "webpageId=" + element.webpageId;
+    url += "&name=" + element.name;
+    url += "&content=" + element.content;
+    url += "&type=" + element.type;
+
+    var url = encodeURI(url);
+    console.log(url);
+    request.open('GET', url);
+    request.onload = saveClick;
+    request.send();
+}
+
+function updateElementCall(element){
+    var url = 'http://localhost/php/cms/back-end/database/EditElement.php?';
+    url += "id=" + element.id;
+    url += "&content=" + element.content;
+    url += "&type=" + element.type;
+
+    var url = encodeURI(url);
+    request.open('GET', url);
+    request.onload = saveClick;
+    request.send();
+}
+
+function deleteElementCall(id){
+    var url = 'http://localhost/php/cms/back-end/database/DeleteElement.php?id=' + id;
+    request.open('GET', url);
+    request.onload = saveClick;
+    request.send();
 }
